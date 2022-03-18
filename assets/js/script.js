409ed62cce9ba4d9c8d8aa0c6bd890de60638e60
NicholasCH <nicholasch96@gmail.com>
2022-03-18 10:12:36 -0500

    post st. pats day refactor continuuing: connect script query finders to html=> then style the stylesheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-const { escapeXML } = require("ejs");
-const { Template } = require("ejs");
+// const { escapeXML } = require("ejs");
+// const { Template } = require("ejs");
 
 var current = document.querySelector("#current");
+
 // var fiveDay = document.querySelector("#5day");
 var searchLabel = document.querySelector("#searchLabel");
+
 var searchInput = document.querySelector("#searchInput");
+
 var submitBtn = document.querySelector("#submitBtn");
+
 var apiKey = "13f6e1ce2c55bbeec150c9d169520dd1"; 
-var 
 //388964fc2ffae47f3a212b1f9aac6d8b  <--Alternate API key may be needed.
 
 
@@ -76,23 +79,28 @@ function dataFetch(city) {
                     var forcastPic = document.createElement('img')
                     var forcastPicFind = el.weather[0].icon;
                     var forcastPicPull = "http://openweathermap.org/img/w/"+ forcastPicFind + ".png";
+
                     var tempEl = document.createElement("p")
                     var humidEl = document.createElement("p")
                     var windEl = document.createElement("p")
                     var uvEl = document.createElement("p")
+
                     var dateFetch = data.dt;
                     var dateMilSecs = dateFetch*1000;
                     var dateUse = new Date(dateMilSecs);
                     var dateDisp = dateUse.toLocalString();
+
                     forcastDiv.id = el.length
                     //set styling for forcast elements: return and verify satisfies preference
                     forcastDiv.className = "card col-2 bg-secondary"
                     h3.innerText = dateDisp;
                     forcastPic.src = forcastPicPull;
+
                     tempEl.textContent = "Tempurature: " + el.temp.day;
                     humidEl.textContent = "Humidity: " + el.humidity/*.day*/;
                     windEl.textContent = "Wind-Speed: " + el.wind_speed/*.day*/;
                     uvEl.textContent = "UV Index: " + el.uvi/*.day*/;
+
                     forcastHead.appendChild(forcastPic);
                     forcastDiv.appendChild(forcastHead);
                     forcastDiv.appendChild(tempEl);
@@ -101,65 +109,50 @@ function dataFetch(city) {
                     forcastDiv.appendChild(uvEl);
                     forcast.appendChild(forcastDiv);
                 }
-            }
-
+            };
         })
     })
-
+.catch(function (err) {
+    console.error(err);
+});
 }
 
-/*submitBtn.addEventListener("click", function(event){
-    var city = searchInput.value
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    console.log(apiURL);
-    event.preventDefault();
+//Previousely Searched Section:
+var historyBar = function(city){
+    if(cityArray.indexOf(city)<0){
+    cityArray.push(city)
+    localStorage.setItem("History", JSON.stringify(cityArray))
+    historySel();
+};
+    console.log(cityArray);
+}
 
-    fetch(apiURL)
-    .then(function (res) {
-        return res.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        //Verify that the user has T
-
-        var currentData = data;
-
-        var api2URL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentData.coord.lat + "&lon=" + currentData.coord.lon + "&appid=" + apiKey;
-        event.preventDefault();
-
-        fetch(api2URL)
-
-        .then(function (res) {
-            return res.json();
+var historySel = function() {
+    historyDiv.innerHTML="";
+    cityArray.forEach(city => {
+        var histClick = document.createElement("button");
+        histClick.className = "mt-1 col-12";
+        histClick.id = city;
+        histClick.innerText = city;
+        histClick.addEventListener("click", function(event) {
+            var city = event.target.id;
+            dataFetch(city);
         })
-
-        .then(function (data2) {
-            console.log(data2);
-        })    
-    })
-
-    .catch(function (err) {
-        console.error(err);
+        historyDiv.appendChild(histClick);
     });
+}
 
-    
-}); */
+historySel();
+
+submitBtn.addEventListener("click", function() {
+    var city = searchInput.value;
+    if (city === "") {
+        alert("Please Enter a City Name");
+        return;
+    }
+    dataFetch(city);
+});
 
 
-/* <body>
-    <header>
-        <h1>Weather; or Not!</h1>
-    </header>
-    <div>
-        <label id="searchLabel">Search here for your city: </label>
-        <input id="searchInput"></input>
-        <container class="searhis">
-            <button id="submitBtn" type="submit">Search</button>
-        </container>
-    </div>
-    <div id="current"></div>
-    <div id="5day"></div>
-    <script src="assets/js/script.js"></script>
-</body> */
 
 
